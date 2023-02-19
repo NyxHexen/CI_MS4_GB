d0539e06cbd1d5c3590b00eadfa6f1631dbf7bf3
NyxHexen <jd.davis948@gmail.com>
2023-02-19 12:56:13 +0000

    Initial commit

---
+++ home/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+import requests
+
+
+def index(request):
+    """ A view to return the index page """
+
+    RAWG_API_KEY = "cf516d7b06284738bf24e8af5f7e4ffc"
+
+    response = requests.get(f"https://api.rawg.io/api/games?key={RAWG_API_KEY}&page_size=40&page=1")
+    print(response.status_code)
+    context = {
+        'response': response,
+    }
+
+    return render(request, 'home/index.html', context)
