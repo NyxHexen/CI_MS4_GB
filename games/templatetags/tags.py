8d8a2e0eca8043f2687bc973f272848aa6615612
NyxHexen <jd.davis948@gmail.com>
2023-03-05 17:35:24 +0000

    CHANGE: refactored to accept an additional parameter to provide either the file name or description

--- games/templatetags/tags.py
+++ games/templatetags/tags.py
@@ -1,12 +1,17 @@
 from django import template
+from django.contrib import messages
 
 register = template.Library()
 
 @register.filter
 def use_media(set, media_use):
+    img_type, img_attr = media_use.split('.')
     try:
         # Change file.name to URL after implementing S3
-        media = set.media.filter(media_use=media_use).first().file.name
+        media = set.media.filter(media_use=img_type).first()
+        if img_attr == 'src':
+            return media.file.name
+        elif img_attr == 'descr':
+            return media.description
     except:
         return None
-    return media
