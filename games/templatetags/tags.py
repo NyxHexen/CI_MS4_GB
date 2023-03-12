c0f36cc80ad3004e540c582d181eda2996b37535
NyxHexen <jd.davis948@gmail.com>
2023-03-12 17:45:42 +0000

    CHANGE: use_media filter now works with promo model too!

--- games/templatetags/tags.py
+++ games/templatetags/tags.py
@@ -8,7 +8,11 @@ def use_media(set, media_use):
     img_type, img_attr = media_use.split('.')
     try:
         # Change file.name to URL after implementing S3
-        media = set.media.filter(media_use=img_type).first()
+        if set.model_name() == 'promo':
+            media = set.media
+        else:
+            media = set.media.filter(media_use=img_type).first()
+        
         if img_attr == 'src':
             return media.file.name
         elif img_attr == 'descr':
