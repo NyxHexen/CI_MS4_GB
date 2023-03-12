8c1dbb910003c7cda2c7bf04e2e5536b9f33fb62
NyxHexen <jd.davis948@gmail.com>
2023-03-12 12:53:37 +0000

    ADD&CHANGE: added logic for carousel, featured and dotd (deals of the day) database query; carousel and featured are now controlled via admin boolean field; added featured games section;  removed green flow from carousel

--- home/views.py
+++ home/views.py
@@ -1,9 +1,41 @@
 from django.shortcuts import render
 from games.models import Game
+import datetime as dt
+
+import random
 
 
 def index(request):
     """ A view to return the index page """
+    carousel = Game.objects.filter(carousel=True)
+    featured = list(Game.objects.filter(featured=True))
+    if len(featured) > 4:
+        featured = random.sample(featured, 4)
+    
+    dotd_queryset = Game.objects.filter(in_promo=True)
+    target_date = dt.date.today() + dt.timedelta(days=2)
+    dotd_nearest = list()
+    iter_count = 0
+    while len(dotd_nearest) < 4:
+        iter_count += 1
+
+        for value in dotd_queryset:
+            if value.promo.end_date.date() <= target_date:
+                if value not in featured and value not in dotd_nearest:
+                    dotd_nearest.append(value)
+
+        if iter_count == 13:
+            break
+        elif iter_count == 8:
+            target_date = target_date + dt.timedelta(days=2)
+            print(target_date)
+        elif iter_count == 4:
+            target_date = target_date + dt.timedelta(days=2)
+            print(target_date)
+
+
     context = {
+        'carousel': carousel,
+        'featured': featured,
     }
     return render(request, 'home/index.html', context)
