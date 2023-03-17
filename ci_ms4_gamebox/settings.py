29d31296b2dd0df1e7644a2906cebb75461e22b5
NyxHexen <jd.davis948@gmail.com>
2023-03-17 22:09:33 +0000

    Revert "CHANGE: added heroku as allowed host"
    This reverts commit e024a013bb6e95195a01f6a25652f238a678e27e.

--- ci_ms4_gamebox/settings.py
+++ ci_ms4_gamebox/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", False)
 
-ALLOWED_HOSTS = ['ci-ms4-gamebox.herokuapp.com',]
+ALLOWED_HOSTS = []
 
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
