401d04fa2311c8b9b8af7d7953b420414e324bcd
NyxHexen <jd.davis948@gmail.com>
2023-03-18 16:36:46 +0000

    CHANGE: refactored DOTD logic to make it more efficient and concise

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Max
 from games.models import Game, DLC
 from promo.models import Promo
 import datetime as dt
@@ -14,31 +15,27 @@ def index(request):
 
     featured = list(Game.objects.filter(featured=True))
     if len(featured) > 4:
-        featured = random.sample(featured, 4)
+        featured = random.sample(featured, 6)
     
-    dotd_queryset = Game.objects.filter(in_promo=True)
-    target_date = dt.date.today() + dt.timedelta(days=2)
-    dotd_nearest = list()
-    iter_count = 0
-    while len(dotd_nearest) < 4:
-        iter_count += 1
+    dotd = list(Promo.objects.filter(active=True, landing_page=True))
 
-        for value in dotd_queryset:
-            if value.promo.end_date.date() <= target_date:
-                if value not in featured and value not in dotd_nearest:
-                    dotd_nearest.append(value)
+    for promo in dotd:
+        promo_games = promo.apply_to_game.all().aggregate(Max('promo_percentage')).get('promo_percentage__max', 0)
+        promo_dlc = promo.apply_to_dlc.all().aggregate(Max('promo_percentage')).get('promo_percentage__max', 0)
+        highest_promo_percentage = max(
+            promo_games if promo_games is not None else 0, 
+            promo_dlc if promo_dlc is not None else 0)
+        promo.promo_percentage = highest_promo_percentage
 
-        if iter_count == 13:
-            break
-        elif iter_count == 8:
-            target_date = target_date + dt.timedelta(days=2)
-        elif iter_count == 4:
-            target_date = target_date + dt.timedelta(days=2)
-
+    if len(dotd) < 4:
+        dotd_topup = list(Game.objects.filter(in_promo=True, promo__active=True, promo__landing_page=False))
+        dotd_topup += list(DLC.objects.filter(in_promo=True, promo__active=True, promo__landing_page=False))
+        dotd_topup = sorted(dotd_topup, key=lambda x: x.promo.end_date)
+        dotd = dotd + dotd_topup[:4 - len(dotd)]
 
     context = {
         'carousel': carousel,
         'featured': featured,
-        'dotd': dotd_nearest
+        'dotd': dotd
     }
     return render(request, 'home/index.html', context)
