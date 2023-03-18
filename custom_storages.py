dcc9ba31114849d2900955716dacfd24fbd24ded
NyxHexen <jd.davis948@gmail.com>
2023-03-18 11:59:15 +0000

    ADD: added a file naming policy to custom_storages so name of uuploaded files is random hex

--- custom_storages.py
+++ custom_storages.py
@@ -1,6 +1,9 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
+import uuid
+import os
+
 
 class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
@@ -8,3 +11,10 @@ class StaticStorage(S3Boto3Storage):
 
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
+
+    def get_valid_name(self, name):
+        # Modify the name of the file being uploaded
+        serial_number = uuid.uuid4().hex[:10]
+        _, ext = os.path.splitext(name)
+        new_name = serial_number + ext
+        return new_name
