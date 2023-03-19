1fb108c8d505a2344bed7383500a5b8ec710802b
NyxHexen <jd.davis948@gmail.com>
2023-03-19 20:23:21 +0000

    ADD: pagination logic and styling for /games/

--- games/views.py
+++ games/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage
 from games.models import Game
 
 
 def games(request):
-    games = Game.objects.all()
+    games = Game.objects.order_by('name')
+    paginator = Paginator(games, 4)
+
+    page_number = request.GET.get('page')
+    try:
+        page  = paginator.get_page(page_number)
+    except EmptyPage:
+        page  = paginator.get_page(paginator.num_pages)
+    paginator_iter = range(1, page.paginator.num_pages + 1)
 
     context = {
-        'games': games
+        'page': page,
+        'paginator_iter': paginator_iter
     }
     return render(request, 'games/index.html', context)
