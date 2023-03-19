fcba2b744048d56b0a30463d8340f9613539e730
NyxHexen <jd.davis948@gmail.com>
2023-03-19 11:18:31 +0000

    ADD: added new manage.py command which is being run every day at 00:30 to auto-set/unset Promo Active state

---
+++ promo/management/commands/check_promo.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from promo.models import Promo
+from django.utils import timezone
+
+
+class Command(BaseCommand):
+    help = "Check the status of all Promos and update their active field accordingly"
+    def handle(self, *args, **options):
+        now = timezone.now()
+        for promo in Promo.objects.all():
+            if promo.start_date <= now <= promo.end_date:
+                promo.active = True
+            else:
+                promo.active = False
+            promo.save()
+            print(f"Promo {promo.name} active: {promo.active}")
