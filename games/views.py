04093406042a988a7a84c5b4697e6ece03134282
NyxHexen <jd.davis948@gmail.com>
2023-03-25 12:51:47 +0000

    CHANGE: populated remainder of games filter accordion

--- games/views.py
+++ games/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage
-from games.models import Game
+from games.models import Game, Genre, Tag, Platform, Feature
 
 
 def games(request):
@@ -14,8 +14,16 @@ def games(request):
         page  = paginator.get_page(paginator.num_pages)
     paginator_iter = range(1, page.paginator.num_pages + 1)
 
+    genres = Genre.objects.all()
+    tags = Tag.objects.all()
+    platforms = Platform.objects.all()
+    features = Feature.objects.all()
     context = {
         'page': page,
-        'paginator_iter': paginator_iter
+        'paginator_iter': paginator_iter,
+        'genres': genres,
+        'tags': tags,
+        'platforms': platforms,
+        'features': features,
     }
     return render(request, 'games/index.html', context)
