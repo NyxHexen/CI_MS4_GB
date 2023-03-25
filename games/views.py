63ec742160b82d9e0cae093b1e8ea50d2e1d6d84
NyxHexen <jd.davis948@gmail.com>
2023-03-25 13:11:50 +0000

    CHANGE: price slider top limit now based on available games (dynamic)

--- games/views.py
+++ games/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage
+from django.db.models import Max
 from games.models import Game, Genre, Tag, Platform, Feature
 
 
@@ -13,11 +14,13 @@ def games(request):
     except EmptyPage:
         page  = paginator.get_page(paginator.num_pages)
     paginator_iter = range(1, page.paginator.num_pages + 1)
+    price_slider_ceil = float(games.aggregate(Max('final_price'))['final_price__max'])
 
     genres = Genre.objects.all()
     tags = Tag.objects.all()
     platforms = Platform.objects.all()
     features = Feature.objects.all()
+    
     context = {
         'page': page,
         'paginator_iter': paginator_iter,
@@ -25,5 +28,6 @@ def games(request):
         'tags': tags,
         'platforms': platforms,
         'features': features,
+        'price_slider_ceil': price_slider_ceil,
     }
     return render(request, 'games/index.html', context)
