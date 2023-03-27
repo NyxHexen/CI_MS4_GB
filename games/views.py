89273ad5dcb24f38d8bb1c6736022e5a6c239e94
NyxHexen <jd.davis948@gmail.com>
2023-03-27 22:46:37 +0100

    ADD: sort functionality javascript and view function

--- games/views.py
+++ games/views.py
@@ -10,8 +10,8 @@ from datetime import datetime
 
 
 def games(request):
-    dlcs = DLC.objects.all()
     games = Game.objects.all()
+    dlcs = DLC.objects.all()
 
     filter_dict = QueryDict(mutable=True)
 
@@ -38,17 +38,25 @@ def games(request):
                 filtered_results_games = value(filtered_results_games, filter_param)
                 filtered_results_dlcs = value(filtered_results_dlcs, filter_param)
 
-        filtered_results = (list(filtered_results_games) if filtered_results_games is not None else list()) + (list(filtered_results_dlcs) if filtered_results_dlcs is not None else list())
+        if "sort_by" in request.GET:
+            filter_dict.update({f'sort_by': f'{request.GET.get("sort_by")}'})
+
+        filtered_results = sort_by(request.GET.get("sort_by"), filtered_results_games, filtered_results_dlcs)
+
         paginator = Paginator(filtered_results, 4)
     else:
-        games = list(games) + list(dlcs)
+        if "sort_by" in request.GET:
+            filter_dict.update({f'sort_by': f'{request.GET.get("sort_by")}'})
+        games = sort_by(request.GET.get("sort_by"), games, dlcs)
         paginator = Paginator(games, 4)
 
     page_number = request.GET.get('page')
+
     try:
         page  = paginator.get_page(page_number)
     except EmptyPage:
         page  = paginator.get_page(paginator.num_pages)
+        
     paginator_iter = range(1, page.paginator.num_pages + 1)
     price_slider_ceil = float(max(games, key=lambda item: item.final_price).final_price)
 
@@ -66,8 +74,33 @@ def games(request):
         'features': features,
         'price_slider_ceil': price_slider_ceil,
     }
-    if "filter" in request.GET:
+    if "filter" in request.GET or "sort_by" in request.GET:
         context['filter_dict'] = urlencode(filter_dict)
         return render(request, 'games/index.html', context)
     else: 
         return render(request, 'games/index.html', context)
+
+
+def sort_by(sort_value, *args):
+    sorted_args = [list(arg) for arg in args] if len(args) != 0 else list()
+    sorted_args = [item for sublist in sorted_args for item in sublist]
+    match sort_value:
+        case "price_desc":
+            sorted_args.sort(key=lambda x: x.final_price)
+        case "price_asc":
+            sorted_args.sort(key=lambda x: x.final_price, reverse=True)
+        case "discount_desc":
+            sorted_args.sort(key=lambda x: x.promo_percentage, reverse=True)
+        case "title_asc":
+            sorted_args.sort(key=lambda x: x.name, reverse=True)
+        case "title_desc":
+            sorted_args.sort(key=lambda x: x.name)
+        case "date_desc":
+            sorted_args.sort(key=lambda x: x.release_date)
+        case "date_asc":
+            sorted_args.sort(key=lambda x: x.release_date, reverse=True)
+        case "rating_desc":
+            sorted_args.sort(key=lambda x: x.rating, reverse=True)
+        case _:
+            print("Wooops!")
+    return sorted_args
