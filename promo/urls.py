e85975d3694c6cefeb1cc699854160396675cd08
NyxHexen <jd.davis948@gmail.com>
2023-04-01 21:42:22 +0100

    ADD: new promo page skeleton plus carousel template

--- promo/urls.py
+++ promo/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.promo, name='promo')
+    path('<promo_id>', views.promo, name='promo')
 ]
--- promo/views.py
+++ promo/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from promo.models import Promo
 
 
-def promo(request):
+def promo(request, promo_id):
+    promo = Promo.objects.get(id=promo_id)
     context = {
+        'promo': promo,
     }
     return render(request, 'promo/index.html', context)
