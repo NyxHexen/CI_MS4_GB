0328e98a0c9fea16a0a344cb349682ca4197aec3
NyxHexen <jd.davis948@gmail.com>
2023-04-02 17:19:36 +0100

    CHANGE: reintroduced refactored logic behind _update_promo_items method to manage add/delete of items in m2m fields; added further checks in save method; concurrency issues in heavy traffic possible?

--- promo/models.py
+++ promo/models.py
@@ -33,16 +33,58 @@ class Promo(CustomBaseModel):
         if self.start_date and self.end_date and self.start_date > self.end_date:
             raise ValidationError('End date cannot be before start date.', code='invalid', params={'end_date': 'End date'})
 
-    pre_add = None
-    post_add = None
-    pre_remove = None
-    post_remove = None
+    def save(self):
+        if self.active:
+            for game in set(self.apply_to_dlc.all()).union(set(self.apply_to_dlc.all())):
+                game.in_promo = True
+                game.promo = self
+                game.save()
+        elif not self.active:
+            for game in set(self.apply_to_dlc.all()).union(set(self.apply_to_dlc.all())):
+                game.in_promo = False
+                game.promo = None
+                game.save()
+        return super().save()
     
-
     def delete(self, using=None, keep_parents=False):
         remove_discount(self)
         return super().delete(using=using, 
                               keep_parents=keep_parents)
+    
+    pre_add = None
+    post_add = None
+    pre_remove = None
+    post_remove = None
+
+
+    # Manage status on adding/removing from M2M field.
+    def _update_promo_items(self, **kwargs):
+        match kwargs["action"]:
+            case 'pre_remove':
+                self.pre_remove = set(self.apply_to_game.all())
+                self.pre_remove = self.pre_remove.union(set(self.apply_to_dlc.all()))
+            case 'post_remove':
+                self.post_remove = set(self.apply_to_game.all())
+                self.post_remove = self.post_remove.union(set(self.apply_to_dlc.all()))
+                for i in self.pre_remove.difference(self.post_remove):
+                    game = get_or_none(kwargs['model'], id=i.id)
+                    game.in_promo = False
+                    game.promo = None
+                    game.percentage = 0
+                    game.save()
+            case 'pre_add':
+                self.pre_add = set(self.apply_to_game.all())
+                self.pre_add = self.pre_add.union(set(self.apply_to_dlc.all()))
+            case 'post_add':
+                self.post_add = set(self.apply_to_game.all())
+                self.post_add = self.post_add.union(set(self.apply_to_dlc.all()))
+                for i in self.post_add.difference(self.pre_add):
+                    game = get_or_none(kwargs['model'], id=i.id)
+                    game.in_promo = True if self.active else False
+                    game.promo = self if self.active else None
+                    game.save()
+            case _:
+                return
     
     def total_in_promo(self):
         return len(self.apply_to_game.all()) + len(self.apply_to_dlc.all())
@@ -58,6 +100,7 @@ def promo_price_apply(instance, *args, **kwargs):
         current = promo.active
         updated = instance.active
         apply_discount(instance) if current < updated else remove_discount(instance) if current > updated else None
+
 
 # Applies the discount to all associated games/DLCs when a Promo becomes active.
 # It calculates the final price and sets the promo and in_promo fields for each game/DLC.
@@ -75,3 +118,13 @@ def remove_discount(instance):
         for item in queryset:
             final_price = item.base_price
             queryset.filter(id=item.id).update(final_price=final_price, promo_percentage=0, in_promo=False, promo=None)
+
+
+@receiver(m2m_changed, sender=Promo.apply_to_game.through)
+def game_change(instance, *args, **kwargs):
+    instance._update_promo_items(**kwargs)
+
+@receiver(m2m_changed, sender=Promo.apply_to_dlc.through)
+def dlc_change(instance, *args, **kwargs):
+    instance._update_promo_items(**kwargs)
+        
