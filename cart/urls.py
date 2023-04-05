a02a6ec28efc33a7a95890ba0a967d7b8de4a8ca
NyxHexen <jd.davis948@gmail.com>
2023-04-05 20:35:21 +0100

    ADD&CHANGE: added cart_add url; created cart_add view logic; added cart button for users or guests in nav; styling for add to cart form

--- cart/urls.py
+++ cart/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_cart, name='cart')
+    path('', views.view_cart, name='cart'),
+    path('add/<model_name>&<game_id>', views.cart_add, name='cart_add'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from games.models import Game, DLC
+from .utils import sign_and_set_cart, get_and_unsign_cart
 
 
 # Create your views here.
@@ -6,3 +8,20 @@ def view_cart(request):
     context = {
     }
     return render(request, 'cart/index.html', context)
+
+
+def cart_add(request, model_name, game_id):
+    if not request.user.is_authenticated:
+        game = Game.objects.filter(id=game_id) if model_name == 'game' else DLC.objects.filter(id=game_id)
+        quantity = int(request.POST.get('quantity'))
+        redirect_url = request.POST.get('redirect_url')
+
+        cart = get_and_unsign_cart(request)
+
+        if game_id in cart:
+            cart[game_id]['quantity'] += quantity
+        else:
+            cart[game_id] = {'model': model_name, 'quantity': quantity }
+
+        sign_and_set_cart(request, cart)
+    return redirect(redirect_url)
