4a2a0f8f12a21dd99cad4e2d97b8f624aab052f7
NyxHexen <jd.davis948@gmail.com>
2023-04-09 22:27:16 +0100

    CHANGE: registered profiles in admin.py; tested it all works, and it does!

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import UserProfile
 
 # Register your models here.
+
+# class ProfilesAdmin(admin.ModelAdmin):
+
+
+
+admin.site.register(UserProfile)
--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
+from .models import UserProfile
+
 # Create your views here.
 def myprofile(request):
+    profile = UserProfile.objects.get(user=request.user)
     context = {
+        'profile': profile,
     }
     return render(request, 'profiles/index.html', context)
