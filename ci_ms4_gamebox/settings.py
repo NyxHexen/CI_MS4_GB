be096152c71f63d38aff1d5592c8cb369a9e0cd7
NyxHexen <jd.davis948@gmail.com>
2023-04-09 21:56:13 +0100

    ADD: created profiles app and basic starting structure

--- ci_ms4_gamebox/settings.py
+++ ci_ms4_gamebox/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'promo',
     'cart',
     'checkout',
+    'profiles',
 
     # Other
     'storages',
--- ci_ms4_gamebox/urls.py
+++ ci_ms4_gamebox/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('promo/', include('promo.urls')),
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
+    path('myprofile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
