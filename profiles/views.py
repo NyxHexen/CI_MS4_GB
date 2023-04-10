7fb8ecbc01189c75fad34024cc6e98b0549b264c
NyxHexen <jd.davis948@gmail.com>
2023-04-10 09:54:06 +0100

    CHANGE: myprofile now requires user to be logged in

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 from .models import UserProfile
 
 # Create your views here.
+@login_required
 def myprofile(request):
     profile = UserProfile.objects.get(user=request.user)
     context = {
