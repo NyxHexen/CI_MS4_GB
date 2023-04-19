dfa2d0a0233d38d6c8c20ed9215fbdbf3517a910
NyxHexen <jd.davis948@gmail.com>
2023-04-19 12:56:18 +0100

    CHANGE: split signals from model and registered in app

--- home/models.py
+++ home/models.py
@@ -1,11 +1,7 @@
 from django.db import models
 from django.utils.text import slugify
-from django.db.models.signals import pre_save
-from django.dispatch import receiver
 
-import os
 
-# Create your models here.
 class CustomBaseModel(models.Model):
     """
     DRY!
@@ -69,45 +65,3 @@ class Media(CustomBaseModel):
 
     def __str__(self) -> str:
         return self.name
-
-
-@receiver(pre_save, sender=Media)
-def receiver(instance, *args, **kwargs):
-
-    if instance.file._file is not None:  # If a file is being uploaded
-        _, new_file_extension = os.path.splitext(instance.file._file.name)
-        new_file_extension = new_file_extension.replace(".", "")
-
-    if instance.file.name is not None and getattr(instance.file, 'name') not in [None, 0]:
-        _, curr_file_extension = os.path.splitext(instance.file.name)
-        curr_file_extension = curr_file_extension.replace(".", "")
-
-
-    print(instance.file.__dict__)
-    if (
-        getattr(instance.file, 'name') not in [None, 0]
-        and instance.media_ext is not None
-        and instance.file._file is None
-    ):
-        # File exists, there is a media extension, and no new file is coming in.
-        if curr_file_extension != instance.media_ext:
-            # Auto-fill media extension.
-            instance.media_ext = curr_file_extension
-    elif instance.media_ext is None:
-        # There is no media extension.
-        if instance.file and instance.file._file is not None:
-            # , but there is a new file coming in.
-            instance.media_ext = new_file_extension
-        elif getattr(instance.file, 'name') not in [None, 0]:
-            # , but there's a file already.
-            instance.media_ext = curr_file_extension
-    elif instance.media_ext is not None and getattr(instance.file, 'name') in [None, 0]:
-        # There is an extension but no file.
-        instance.media_ext = None
-    elif instance.media_ext is not None and instance.file._file is not None:
-        # There is an extension, but a new file is coming in.
-        instance.media_ext = new_file_extension
-    else:
-        # None of the above applied.
-        # If a cover photo exists already, don't save."
-        pass
