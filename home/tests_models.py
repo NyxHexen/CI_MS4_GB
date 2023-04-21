88ed0582b6c382bf2d6b140bd190b5007586f5bb
NyxHexen <jd.davis948@gmail.com>
2023-04-21 17:24:52 +0100

    ADD: created and populated model tests

---
+++ home/tests_models.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+from .models import Media
+
+class MediaModelTest(TestCase):
+    
+    def setUp(self):
+        Media.objects.create(
+            name='Test Media',
+            url='',
+            media_type='image',
+            media_use='COVER',
+            media_ext='png',
+            description='Test media description'
+        )
+
+    def test_media_name(self):
+        media = Media.objects.get(name='Test Media')
+        self.assertEqual(media.__str__(), media.name)
+    
+    def test_media_slug(self):
+        media = Media.objects.get(name='Test Media')
+        self.assertEqual(media.slug, 'test-media')
