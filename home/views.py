daa0f0034653fdc497bc81a5890e36900098cc9f
NyxHexen <jd.davis948@gmail.com>
2023-04-22 11:55:37 +0100

    CHANGE: added extra try/except to home view; added empty placeholders to carousel

--- home/views.py
+++ home/views.py
@@ -13,32 +13,43 @@ import random
 
 def index(request):
     """A view to return the index page"""
-    carousel = list(Game.objects.filter(carousel=True))
-    carousel += list(DLC.objects.filter(carousel=True))
+    try:
+        carousel = list(Game.objects.filter(carousel=True))
+        carousel += list(DLC.objects.filter(carousel=True))
+    except Exception:
+        carousel = []
 
-    is_featured = carousel
-    if len(is_featured) > 4:
-        cards_num = 5 if request.user_agent.is_pc else 4
-        is_featured = random.sample(is_featured, cards_num)
+    try: 
+        featured_games = list(Game.objects.filter(is_featured=8))
+        featured_games += list(DLC.objects.filter(is_featured=8))
+        if len(featured_games) > 4:
+            cards_num = 5 if request.user_agent.is_pc else 4
+            is_featured = random.sample(featured_games, cards_num)
+    except Exception:
+        is_featured = []
+    
+    try:
+        carousel += list(Promo.objects.filter(carousel=True))
+    except Exception:
+        pass
 
-    carousel += list(Promo.objects.filter(carousel=True))
-
-    dotd = list(Promo.objects.filter(active=True, landing_page=True))
-
-    if len(dotd) < 4:
-        dotd_topup = list(
-            Game.objects.filter(
-                in_promo=True, promo__active=True, promo__landing_page=False
+    try:
+        dotd = list(Promo.objects.filter(active=True, landing_page=True))
+        if len(dotd) < 4:
+            dotd_topup = list(
+                Game.objects.filter(
+                    in_promo=True, promo__active=True, promo__landing_page=False
+                )
             )
-        )
-        dotd_topup += list(
-            DLC.objects.filter(
-                in_promo=True, promo__active=True, promo__landing_page=False
+            dotd_topup += list(
+                DLC.objects.filter(
+                    in_promo=True, promo__active=True, promo__landing_page=False
+                )
             )
-        )
-        dotd_topup = sorted(dotd_topup, key=lambda x: x.promo.end_date)
-        dotd = dotd + dotd_topup[: 4 - len(dotd)]
-
+            dotd_topup = sorted(dotd_topup, key=lambda x: x.promo.end_date)
+            dotd = dotd + dotd_topup[: 4 - len(dotd)]
+    except Exception:
+        dotd = []
     context = {"carousel": carousel, "is_featured": is_featured, "dotd": dotd}
     return render(request, "home/index.html", context)
 
