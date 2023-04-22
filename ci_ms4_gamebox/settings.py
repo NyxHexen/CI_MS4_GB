c17c0ae4aee07d3526fbb8ecea296221f6f9a0f8
NyxHexen <jd.davis948@gmail.com>
2023-04-22 12:33:24 +0100

    UPDATE: requirements.txt

--- ci_ms4_gamebox/settings.py
+++ ci_ms4_gamebox/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEVELOPMENT', False)
 
 ALLOWED_HOSTS = ['ci-ms4-gamebox.herokuapp.com', 'localhost', '127.0.0.1']
 
